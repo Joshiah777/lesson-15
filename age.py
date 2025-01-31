29f418f1ba121c46b1bec35128f52dc1e7818c84
Joshiah777 <Joshiahpthomas@gmail.com>
2025-01-31 17:07:15 +0530

    demo

---
+++ age.py
@@ -0,0 +1,10 @@
+age=input("What is your age:")
+if age>=10:
+    print("You are a child.")
+else:
+    if age>=18:
+        print("You are a teenager.") 
+    else:
+        print("You are a mature adult.")
+
+    
